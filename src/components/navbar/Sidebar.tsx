c33efdc9a0c64d3a21054cba20766b93f7d14bcc
Cristina <cristinabosasanchez@gmail.com>
2024-08-17 12:40:02 +0100

    Se ha ampliado los iconos del menu lateral + añadido el símbolo de la marca

--- src/components/navbar/Sidebar.tsx
+++ src/components/navbar/Sidebar.tsx
@@ -1,49 +1,53 @@
 "use client"
 
 import { usePathname } from "next/navigation";
-import { LayoutDashboard, HeartPulse, CalendarCheck, User } from "lucide-react";
+import { LayoutDashboard, HeartPulse, CalendarCheck, User} from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 const Sidebar = () => {
   const pathname = usePathname();
   return (
     <aside className="fixed top-0 left-0 w-auto h-full" aria-label="Sidenav">
-      <div className="overflow-y-auto py-5 px-3 h-full border-r-2 border-primary">
-        <ul className="space-y-2">
+      <section className="overflow-y-auto py-5 px-3 h-full border-r-2 border-primary">
+        <section>
+          <Image src="/symbol.svg" alt="brand vitalia" width={54} height={54}/>
+        </section>
+        <ul className="space-y-2 mt-4">
           <li>
             <Link 
               href="/patient/dashboard"
-              className={`flex items-center p-2 text-base font-normal ${pathname === '/patient/dashboard' ? 'text-primary-darker bg-primary-lighter' : 'text-primary'} rounded-lg hover:bg-primary hover:text-light`}
+              className={`flex items-center p-2 text-base font-normal ${pathname === '/patient/dashboard' ? 'text-primary-darker bg-primary-lighter' : 'text-primary-darker'} rounded-lg hover:bg-primary hover:text-light`}
             >
-              <LayoutDashboard className="w-6 h-6" />
+              <LayoutDashboard size={32}/>
             </Link>
           </li>
           <li>
             <Link 
               href="/patient/historical"
-              className={`flex items-center p-2 text-base font-normal ${pathname === '/patient/historical' ? 'text-primary-darker bg-primary-lighter' : 'text-primary'} rounded-lg hover:bg-primary hover:text-light`}
+              className={`flex items-center p-2 text-base font-normal ${pathname === '/patient/historical' ? 'text-primary-darker bg-primary-lighter' : 'text-primary-darker'} rounded-lg hover:bg-primary hover:text-light`}
             >
-              <HeartPulse className="w-6 h-6" />
+              <HeartPulse size={32} />
             </Link>
           </li>
           <li>
             <Link 
               href="/patient/appointment"
-              className={`flex items-center p-2 text-base font-normal ${pathname === '/patient/appointment' ? 'text-primary-darker bg-primary-lighter' : 'text-primary'} rounded-lg hover:bg-primary hover:text-light`}
+              className={`flex items-center p-2 text-base font-normal ${pathname === '/patient/appointment' ? 'text-primary-darker bg-primary-lighter' : 'text-primary-darker'} rounded-lg hover:bg-primary hover:text-light`}
             >
-              <CalendarCheck className="w-6 h-6" />
+              <CalendarCheck size={32} />
             </Link>
           </li>
           <li>
             <Link 
               href="/patient/profile"
-              className={`flex items-center p-2 text-base font-normal ${pathname === '/patient/profile' ? 'text-primary-darker bg-primary-lighter' : 'text-primary'} rounded-lg hover:bg-primary hover:text-light`}
+              className={`flex items-center p-2 text-base font-normal ${pathname === '/patient/profile' ? 'text-primary-darker bg-primary-lighter' : 'text-primary-darker'} rounded-lg hover:bg-primary hover:text-light`}
             >
-              <User className="w-6 h-6" />
+              <User size={32} />
             </Link>
           </li>
         </ul>
-      </div>
+      </section>
     </aside>
   );
 };
