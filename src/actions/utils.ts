25767141c35ac8bca57ced63ab67b9b816560eb4
Cristina <cristinabosasanchez@gmail.com>
2024-09-02 17:51:24 +0100

    fixed endpoint utils

--- src/actions/utils.ts
+++ src/actions/utils.ts
@@ -2,21 +2,15 @@ import { baseUrl } from "@/constants";
 import { fetchData } from "./fetch";
 
 export async function fetchGenre() {
-  const response = await fetch(`${baseUrl}/auth/choices/genre`);
-  const data = await response.json();
-  return data;
+  return fetchData(`auth/choices/genre`);  
 }
 
 export async function fetchCity() {
-  const response = await fetch(`${baseUrl}/auth/city`);
-  const data = await response.json();
-  return data;
+  return fetchData(`auth/city`);
 }
 
 export async function fetchSpecialty() {
-  const response = await fetch(`${baseUrl}/auth/specialty`);
-  const data = await response.json();
-  return data;
+  return fetchData(`auth/specialty`); 
 }
 
 export const fetchAllergies = async () => {
